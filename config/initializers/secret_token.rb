68a2e909e28cffc40d65dd7eaad2a11d443fbfce
PatSaysNow <pdberlin89@googlemail.com>
2014-01-07 12:18:34 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HausarbeitAyLin::Application.config.secret_key_base = '0f7398e513ebd595df5c18aedbca68cc4d1a97b9eddc8cfe93523226a0fbc9cd5f9752afb190d6b704b983fc5429564cd223b38a0a8d3a3db753f0aefd3ae2d9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HausarbeitAyLin::Application.config.session_store :cookie_store, key: '_Hausarbeit_Ay_lin_session'
