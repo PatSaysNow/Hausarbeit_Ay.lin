eddf8053353831550457dbca1ed5175e8b96b1b0
PatSaysNow <pdberlin89@googlemail.com>
2014-01-15 10:48:30 +0100

    STAND 15.01.12

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 HausarbeitAyLin::Application.routes.draw do
   get "home/index"
   get "home/about"
+  #get  "home/alleprodukte"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
