3a9ffce3284862f5933b06e13a76b2906da8e7a6
PatSaysNow <pdberlin89@googlemail.com>
2014-01-21 14:21:12 +0100

    NEU

--- db/schema.rb
+++ db/schema.rb
@@ -11,39 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108140606) do
-
-  create_table "categories", force: true do |t|
-    t.string   "name"
-    t.string   "top_category"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "products", force: true do |t|
-    t.string   "name"
-    t.integer  "product_id"
-    t.string   "title"
-    t.float    "rating"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "ratings", force: true do |t|
-    t.integer  "product_id"
-    t.float    "vote"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: true do |t|
-    t.string   "name"
-    t.string   "username"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "password"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
