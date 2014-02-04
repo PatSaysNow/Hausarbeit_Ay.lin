7fe0a489ff56dab70948cb360ab5e51cec7a3588
aylinweiss <aylin.weiss.AW@googlemail.com>
2014-02-04 10:31:13 +0100

    hab mal etwas versucht

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,30 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def new
+	  @product = Product.new
+  end
+
+  def create
+	  @product = Product.new(painting_params)
+
+	  respond_to do |format|
+		  if @product.save
+			  format.html { redirect_to @product, notice: 'Painting was successfully created.' }
+			  format.json { render action: 'show', status: :created, location: @product }
+		  else
+			  format.html { render action: 'new' }
+			  format.json { render json: @product.errors, status: :unprocessable_entity }
+		  end
+	  end
+  end
+
 def painting_params
-  params.require(:painting).permit(:name, :image, :remote_image_url)
+  params.require(:@product).permit(:name, :image, :remote_image_url)
 end
 
   def about
   end
 end
+
